39e88b17ac009cdb65d4d9e082d178dfb9f7a01a
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-22 17:48:37 +0200

    render Image component

--- my-app/src/components/Booklist.js
+++ my-app/src/components/Booklist.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Booklist() {
-  return <section>This is a booklist.</section>;
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg"
+    alt="How Rich People Think"
+  />
+);
