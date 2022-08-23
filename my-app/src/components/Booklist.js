fc279ba60f97e550986366d74fb598fd67df5daf
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-23 12:48:45 +0200

    remove components and add to main book component

--- my-app/src/components/Booklist.js
+++ my-app/src/components/Booklist.js
@@ -15,27 +15,19 @@ export default function Booklist() {
 const Book = () => {
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg"
+        alt="How Rich People Think"
+      />
+      <h1>How Rich People Think</h1>
+      <h4>Steve Siebold</h4>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg"
-    alt="How Rich People Think"
-  />
-);
-
-const Title = () => <h1>How Rich People Think</h1>;
 /* the {} inside of the {} just means there's an object. The first {} indicates
 we are using JS inside of the JSX and the second {} simply means an object. 
 There is really no special syntax here.
 */
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Steve Siebold
-  </h4>
-);
+
+//inline styling is more powerful than CSS coming from index.css
