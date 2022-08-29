ad1526a472a205ea0c45f2fe195acfbf1e265b60
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-29 13:37:38 +0200

    add props to book

--- my-app/src/components/Booklist.js
+++ my-app/src/components/Booklist.js
@@ -17,8 +17,21 @@ export default function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return "hello";
+        const { img, title, author } = book;
+        return <Book book={book}> </Book>;
       })}
     </section>
   );
 }
+
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props.book;
+  return (
+    <article className="book">
+      <img src={img} alt=""></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
