b7a4a43eaad1044a9499a1db424ad7b3514afd79
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-30 12:33:29 +0200

    add key id to each book

--- my-app/src/components/Booklist.js
+++ my-app/src/components/Booklist.js
@@ -2,16 +2,19 @@ import React from "react";
 
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg",
     title: "How Rich People Think",
     author: "Steve Siebold",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
     title: "Atomic Habits",
     author: "James Clear",
   },
   {
+    id: 3,
     img: "https://m.media-amazon.com/images/I/81ZLwAwD-ML._AC_UL640_FMwebp_QL65_.jpg",
     title: "Rich Dad Poor Dad",
     author: "Robert T. Kiyosaki",
@@ -22,8 +25,11 @@ export default function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book book={book}> </Book>;
+        return (
+          <Book key={book.id} book={book}>
+            {" "}
+          </Book>
+        );
       })}
     </section>
   );
