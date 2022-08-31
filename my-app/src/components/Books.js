c02e6f95bcba0c99b15315345064aa054cdf1fb8
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-31 11:55:31 +0200

    set up export books.js

---
+++ my-app/src/components/Books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg",
+    title: "How Rich People Think",
+    author: "Steve Siebold",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/81ZLwAwD-ML._AC_UL640_FMwebp_QL65_.jpg",
+    title: "Rich Dad Poor Dad",
+    author: "Robert T. Kiyosaki",
+  },
+];
