902963aabaf1af369225146702ea9f78f6580d50
ShannonGW <ShannonWilliams93@gmail.com>
2022-08-31 11:58:26 +0200

    import books.js to Booklist.js

--- my-app/src/components/Booklist.js
+++ my-app/src/components/Booklist.js
@@ -1,25 +1,6 @@
 import React from "react";
 
-const books = [
-  {
-    id: 1,
-    img: "https://m.media-amazon.com/images/I/41iBc3SbWBL.jpg",
-    title: "How Rich People Think",
-    author: "Steve Siebold",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
-    title: "Atomic Habits",
-    author: "James Clear",
-  },
-  {
-    id: 3,
-    img: "https://m.media-amazon.com/images/I/81ZLwAwD-ML._AC_UL640_FMwebp_QL65_.jpg",
-    title: "Rich Dad Poor Dad",
-    author: "Robert T. Kiyosaki",
-  },
-];
+import { books } from "./books";
 
 export default function Booklist() {
   return (
